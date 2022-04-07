485b75f2808cd48cce21741cc2b917d195f90943
Samoina <samoina.w@gmail.com>
2022-04-07 10:12:34 +0300

    Add JS for form submission and add book to table

---
+++ app.js
@@ -0,0 +1,42 @@
+//Book constructor function
+function Book(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
+
+//UI constructor function 
+function UI() {};
+
+UI.prototype.addBookToList = function(book){
+  const bookList = document.getElementById('book-list');
+  let tr = document.createElement('tr');
+  let bookTemplate='';
+
+  bookTemplate = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><button class="btn btn-danger">X</button></td>
+  `;
+
+  tr.innerHTML += bookTemplate;
+  bookList.append(tr);
+}
+
+//EVENT LISTENERS
+//form submission
+document.getElementById('book-form').addEventListener("submit", function (ev) {
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const isbn = document.getElementById('isbn').value;
+
+  let book = new Book(title, author, isbn);
+
+  let ui = new UI();
+  ui.addBookToList(book);
+
+  // console.log(ui);
+
+  ev.preventDefault();
+});
