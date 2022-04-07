4180e2665e4c4fc250179ce668da8edfd044fa45
Samoina <samoina.w@gmail.com>
2022-04-07 11:31:43 +0300

    Delete a book from the list and notify the user

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ UI.prototype.addBookToList = function(book){
     <td>${book.title}</td>
     <td>${book.author}</td>
     <td>${book.isbn}</td>
-    <td><button class="btn btn-danger">X</button></td>
+    <td><button class="delete btn btn-danger">X</button></td>
   `;
 
   tr.innerHTML += bookTemplate;
@@ -42,8 +42,14 @@ UI.prototype.addAlert = function(message, alertClass) {
   setTimeout(function(){
     alertDiv.style.display = 'none';
   }, 3000)
+}
 
-
+UI.prototype.deleteBook = function(target) {
+  if(target.classList.contains('delete')) {
+    const bookList = document.getElementById('book-list');
+    let row = target.parentElement.parentElement;
+    bookList.removeChild(row);
+  }
 }
 
 //EVENT LISTENERS
@@ -58,11 +64,21 @@ document.getElementById('book-form').addEventListener("submit", function (ev) {
 
   //validation
   if(title==='' || author==='' || isbn==='') {    
-    ui.addAlert('Please enter all the book fields', 'danger');
+    ui.addAlert('Please enter all the book fields.', 'danger');
   } else {
     ui.addBookToList(book);
     ui.clearFields();
+    ui.addAlert('Book added successfully! <i class="fas fa-smile"></i>', 'success')
   }
 
   ev.preventDefault();
+});
+
+
+//delete a book
+document.getElementById('book-list').addEventListener('click', function(event){
+  let ui = new UI();
+  ui.deleteBook(event.target);
+  ui.addAlert('Book deleted!', 'warning');
+  event.preventDefault();
 });
